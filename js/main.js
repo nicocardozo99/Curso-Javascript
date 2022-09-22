7095a50d33e8e20d7860140b908f498b4e1158aa
nico cardozo <nico_ncardozo@hotmail.com>
2022-09-22 17:14:27 -0300

    Primer entrega

--- js/main.js
+++ js/main.js
@@ -1,49 +1,88 @@
-function saludar() {
-  alert("Bienvenido a Pna Burguers");
-  let nombre = prompt("Ingrese su nombre");
-  while (nombre === "") {
-    nombre = prompt("Ingrese su nombre");
+class producto {
+  constructor(id, nombre, precio) {
+    this.id = id,
+    this.nombre = nombre,
+    this.precio = Number(precio);
   }
-}
+};
 
-function mostrarProductos() {
-  let productoId;
+const arrayProductos = [];
+
+const producto1 = new producto(1, 'Bacon & Cheese', 1200);
+const producto2 = new producto(2, 'Veggie', 800);
+const producto3 = new producto(3, 'Cheeseburguer', 1000);
+
+arrayProductos.push(producto1, producto2, producto3);
+
+const ordenarMenorMayor = () => {
+  arrayProductos.sort((a,b) => a.precio - b.precio);
+  listaOrdenada();
+};
+
+const ordenarMayorMenor = () => {
+  arrayProductos.sort((a,b) => b.precio - a.precio);
+  listaOrdenada();
+};
+
+const listaOrdenada = () => {
+  array = [];
+  arrayProductos.forEach(producto => array.push(producto.nombre+' $'+ producto.precio));
+  alert('Lista de productos: '+'\n'+array.join('\n'));
+};
+
+const agregarProductos = () => {
+  let otroProducto = false;
+  let productoNombre = '';
+  let productoCantidad = 0;
+  let total = 0;
+
   do {
-    productoId = prompt(
-      "Que hamburguesa desea comprar? : \n1) Cheese & bacon\n2) Veggie\n3)Cheese burguer"
-    );
-  } while (productoId < 1 || productoId > 3);
+    productoNombre = prompt('Que hamburguesa desea comprar?');
+    productoCantidad = prompt('Cuantas queres comprar?');
 
-  switch (productoId) {
-    case "1":
-      return "Cheese & bacon";
-    case "2":
-      return "Veggie";
-    case "3":
-      return "Cheese burguer";
+    const producto = arrayProductos.find(producto => producto.nombre === productoNombre);
+
+    if (producto) {
+      total += producto.precio * productoCantidad;
+    } else {
+      alert('El producto no se encuentra en nuestro menu')
+    }
+
+      otroProducto = confirm('Desea seguir comprando?');
+  } while (otroProducto);
+
+  obtenerDescuento(total);
+};
+
+const obtenerDescuento = (total) => {
+  if (total >=3000) {
+    total = total * 0.80;
+    alert('Tenes un descuento del 20% !');
+  }
+  obtenerPrecioDeEnvio(total);
+};
+
+const obtenerPrecioDeEnvio = (total) => {
+  let confirmacion = confirm('Queres envio a domicilio?');
+  if (confirmacion && total >= 4000) {
+    alert('Tenes envio gratis!');
+  } else if (confirmacion && total < 4000 && total !== 0) {
+    total = total + 700;
+    alert('El envio cuesta $700. El total es de: $'+total);
+  } else {
+    alert('El total de tu compra es de: $'+total);
   }
 }
 
-function validarPrecio(nombre) {
-  if (nombre === "Cheese & bacon") {
-    return 1100;
-  } else if (nombre === "Veggie") {
-    return 800;
-  } else if (nombre === "Cheese burguer") {
-    return 1000;
+const lista = () => {
+  if (confirm('Desea ordenar los productos del mas barato al mas caro?')) {
+    ordenarMenorMayor();
   }
+  else {
+    ordenarMayorMenor();
 }
 
-function cobrarProducto(nombre, precio) {
-  alert("Hamburguesa : " + nombre + ".\nEl total a pagar es $" + precio);
-  let pago = prompt("Con cuanto abona?");
+agregarProductos();
+};
 
-  if (precio < pago) {
-    alert("Su vuelto es $" + (pago - precio));
-  }
-}
-
-saludar();
-let nombreProducto = mostrarProductos();
-let precioProducto = validarPrecio(nombreProducto);
-cobrarProducto(nombreProducto, precioProducto);
+lista();
